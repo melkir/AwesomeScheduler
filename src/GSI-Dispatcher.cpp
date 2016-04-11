282ba9a1d5d9ef7b80b53177bfbd145d9839e97f
melkir <melkir13@gmail.com>
2016-04-11 22:09:47 +0200

    Simple Dispatcher and EndUser OK !

--- src/GSI-Dispatcher.cpp
+++ src/GSI-Dispatcher.cpp
@@ -8,59 +8,90 @@
 //----------------------------------------------------------------------
 //----------------------------------------------------------------------
 
-
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <zconf.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <strings.h>
+#include <cassert>
+#include <netdb.h>
+#include <cstring>
 
 using namespace std;
 
 namespace {
 
     void doProcessing(int sock) {
+        char buffer[256];
 
+        /* Read the client message */
+        ssize_t read_count = read(sock, buffer, 255);
+        assert(read_count >= 0 && "read()");
+
+        /* And print it in the console */
+        printf("Server receive : %s\n", buffer);
+
+        /* Send him an ACK */
+        ssize_t write_count = write(sock,"I got your message",18);
+        assert(write_count >= 0 && "write()");
     }
 
     void exec() {
-        int sockfd, newsockfd, port;
-        struct sockaddr_in serv_addr, client_addr;
-        char buffer[256];
+        /* Server address and port number */
+        struct hostent *h = gethostbyname("localhost");
+        assert(NULL != h && "gethostbyname()");
+        uint16_t num_port = 5001;
 
-        /* First call to socket() function */
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        /* Create socket */
+        int theConnection = socket(AF_INET, SOCK_STREAM, 0);
+        assert(theConnection >= 0 && "socket()");
 
-        /* Initialize socket structure */
+        /* Initialize the server socket structure */
+        struct sockaddr_in serv_addr;
         bzero((char *) &serv_addr, sizeof(serv_addr));
-        port = 5000;
         serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = INADDR_ANY;
-        serv_addr.sin_port = htons((uint16_t) port);
+        memcpy(&serv_addr.sin_addr, h->h_addr, 4);
+        serv_addr.sin_port = htons(num_port);
 
-        /* Now bind the host address using bind() call */
-        bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+        /* Bind the host address */
+        if (bind(theConnection, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+            perror("bind() failed");
+            exit(1);
+        }
 
-        /* Now start listening for the clients, here process will
+        /* Start listening for the clients, here process will
          * go in sleep mode and will wait for the incoming connection
          */
-        listen(sockfd, 5);
+        if (listen(theConnection, 5) < 0) {
+            perror("listen() failed");
+            exit(1);
+        }
 
+        int theConversation;
+        struct sockaddr_in client_addr;
+        /* Handle multiple simultaneous connections */
         while (true) {
-            newsockfd = accept(sockfd, (sockaddr *) &client_addr, (socklen_t *) sizeof(&client_addr));
+            socklen_t size_s = sizeof(client_addr);
+            theConversation = accept(theConnection, (sockaddr *) &client_addr, &size_s);
+            if (theConversation < 0) {
+                perror("accept() failed");
+                exit(1);
+            }
             /* Create child process */
             switch (fork()) {
                 case -1:
-                    perror("ERROR on fork");
+                    perror("fork() failed");
                     exit(1);
                 case 0:
                     /* This is the client process */
-                    close(sockfd);
-                    doProcessing(newsockfd);
+                    close(theConnection);
+                    doProcessing(theConversation);
                     exit(0);
                 default:
                     /* This is the parent process */
-                    close(newsockfd);
+                    close(theConversation);
             }
         } /* end of while */
     }
--- src/GSI-EndUser.cpp
+++ src/GSI-EndUser.cpp
@@ -9,13 +9,70 @@
 //----------------------------------------------------------------------
 
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <netinet/in.h>
+#include <strings.h>
+#include <cassert>
+#include <netdb.h>
+#include <cstring>
+#include <zconf.h>
 
 using namespace std;
 
+namespace {
 
+    void doProcessing(int sock) {
+        /* Ask for a message from the user, this message will be read by the server */
+        char buffer[256];
+        printf("Please enter the message: ");
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
+
+        /* Send the message to the server */
+        ssize_t write_count = write(sock, buffer, strlen(buffer));
+        assert(write_count >= 0 && "write()");
+
+        /* Read the server response */
+        bzero(buffer, 256);
+        ssize_t read_count = read(sock, buffer, 255);
+        assert(read_count >= 0 && "read()");
+
+        /* And print it in the console */
+        printf("%s\n", buffer);
+    }
+
+    void exec() {
+        /* Server address and port number */
+        struct hostent *h = gethostbyname("localhost");
+        assert(NULL != h && "gethostbyname()");
+        uint16_t num_port = 5001;
+
+        /* Create socket */
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        assert(sockfd >= 0 && "socket()");
+
+        /* Initialize the server socket structure */
+        struct sockaddr_in serv_addr;
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        memcpy(&serv_addr.sin_addr, h->h_addr, 4);
+        serv_addr.sin_port = htons(num_port);
+
+        /* Connect to the server */
+        int connect_return = connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+        assert(connect_return >= 0 && "connect()");
+
+        doProcessing(sockfd);
+    }
+}
 
 int main(int argc, char *argv[]) {
     cout << "-----------------------\n"
     << " End-User as a Client: \n"
     << "-----------------------" << endl;
+
+    exec();
+
+    return 0;
 }
