dd63359295847748e9a728202b8ee9f572ec3601
melkir <melkir13@gmail.com>
2016-04-13 01:15:06 +0200

    Dispatcher and EndUser refractoring + CMakeList

---
+++ src/EndUser.cpp
@@ -0,0 +1,58 @@
+#include <sys/socket.h>
+#include <Util.h>
+#include <zconf.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <strings.h>
+#include <cstring>
+#include "EndUser.h"
+
+using namespace std;
+
+EndUser::EndUser(const char *hostname, const uint16_t port) : socket(hostname, port) {
+    cout << "--------------------\n"
+    << "End-User as a Client\n"
+    << "\t" << hostname << ":" << port << "\n"
+    << "--------------------" << endl;
+    startClient();
+}
+
+void EndUser::startClient() {
+    const int &sockfd(socket.getSockFD());
+    const sockaddr_in &socketAddr(socket.getSocketAddr());
+
+    /* Connect to the server */
+    int connect_return = connect(sockfd, (struct sockaddr *) &socketAddr, sizeof(socketAddr));
+    Util::myAssert(connect_return >= 0, "connect()");
+
+    cout << "Connection established with the server" << endl;
+
+    doProcessing(sockfd);
+    close(sockfd);
+}
+
+void EndUser::doProcessing(int sock) {
+    /* Ask for a message from the user, this message will be read by the server */
+    char buffer[256];
+    printf("Please enter the message: ");
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
+
+    /* Send the message to the server */
+    ssize_t write_count = write(sock, buffer, strlen(buffer));
+    Util::myAssert(write_count >= 0, "write()");
+
+    /* Read the server response */
+    bzero(buffer, 256);
+    ssize_t read_count = read(sock, buffer, 255);
+    Util::myAssert(read_count >= 0, "read()");
+
+    /* And print it in the console */
+    printf("%s\n", buffer);
+}
+
+int main() {
+    EndUser();
+}
+
+
